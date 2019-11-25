0de95226498d1399a45f537c4453318a223ead79
samuele-riva <s.riva72@campus.unimib.it>
2019-11-25 21:38:03 +0100

    initial commit: aggiunto properties <maven.compile.source>1.8 gson

---
+++ quickstart/src/main/java/samuele/riva/quickstart/App.java
@@ -0,0 +1,13 @@
+package samuele.riva.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
